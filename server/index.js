fb55e971181d5d13fabd44e241337722f55b2d4c
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2023-07-31 18:53:50 +0800

    Update Index.js Backend -Jl

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const passport = require("passport");
+const passportSetup = require("./passport");
+const authRoute = require("./routes/auth");
 require('dotenv').config();
 
 const app = express();
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,6 +29,8 @@ app.use("/bikestop", bikestopRoute);
 const fileRoute = require('./routes/file');
 app.use("/file", fileRoute);
 
+const userRoute = require('./routes/user');
+app.use("/user", userRoute);
 const db = require('./models');
 db.sequelize.sync({ alter: true }).then(() => {
     let port = process.env.APP_PORT;
