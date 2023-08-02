47f64c83e322ee5c688648022a10e735621b8bcc
ZhiLong25 <tanzhilong25@gmail.com>
2023-08-03 02:14:02 +0800

    Split Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -39,6 +39,9 @@ import "./App.css";
 import logo from "./pages/images/powerlogo.png";
 import human from "./pages/images/humanicon.png";
 
+import UserSideNavigation from "./UserSideNavigation"; 
+import AdminSideNavigation from "./AdminSideNavigation"; 
+
 import {
   Button,
   Container,
@@ -63,10 +66,8 @@ function App() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
   const [isDeleted, setIsDeleted] = useState(false);
   // Hook from react-router-dom
+  const [isAdmin, setIsAdmin] = useState(false);
 
-
-  // const isAdmin = user && user.role === False;
-  const isAdmin = false;
   // Function to handle link click and close the navigation menu
   const handleLinkClick = () => {
     setIsNavExpanded(false);
@@ -101,8 +102,9 @@ function App() {
       http.get("/user/auth").then((res) => {
         setUser(res.data.user);
         setUserid(res.data.userid);
+        // Check if the user is an admin
+        setIsAdmin(res.data.user.admin === true);
       });
-
     }
   }, []);
 
@@ -131,166 +133,42 @@ function App() {
     window.location = "/";
   };
 
+
+
+
   return (
     <UserContext.Provider value={{ user, setUser }}>
       <Router>
-        <nav className="navigation">
-          <Link to="/home" className="brand-name">
-            <img
-              src={logo}
-              className="logo"
-              style={{ marginTop: "20px", width: "80%" }}
-              alt="Logo"
-            />
-          </Link>
-          <button
-            className="hamburger"
-            onClick={() => setIsNavExpanded(!isNavExpanded)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="white"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-
-          <div
-            className={
-              isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-            }
-          >
-            <ul>
-              <li>
-                <Link to="/home" onClick={handleLinkClick} className="navlink">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/bikeservice"
-                  onClick={handleLinkClick}
-                  className="navlink"
-                >
-                  Bicycles
-                </Link>
-              </li>
-              <li>
-                <Link to="/" onClick={handleLinkClick} className="navlink">
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link to="/faq" onClick={handleLinkClick} className="navlink">
-                  FAQ
-                </Link>
-              </li>
-              {user && (
-                <>
-                  <li>
-                    <Dropdown className="navlink" style={{ marginTop: "10px" }}>
-                      <Dropdown.Toggle
-                        variant="success"
-                        id="dropdown-basic"
-                        style={{ background: "none", border: "none" }}
-                      >
-                        <img src={human} className="humannlogo" alt="" />
-                      </Dropdown.Toggle>
-
-                      <Dropdown.Menu>
-                        <Dropdown.Item>More...</Dropdown.Item>
-                        <Dropdown.Item href="/securitydetails/:id">
-                          Security Details
-                        </Dropdown.Item>
-                        <Dropdown.Item href="/userdetails/:id">
-                          User Details
-                        </Dropdown.Item>
-                        <Dropdown.Item onClick={logout}>logout</Dropdown.Item>
-                        <Dropdown.Item onClick={handleOpen}>
-                          Delete account
-                        </Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </li>
-                </>
-              )}
-              {!user && (
-                <>
-                  <li>
-                    <Dropdown className="navlink" style={{ marginTop: "10px" }}>
-                      <Dropdown.Toggle
-                        variant="success"
-                        id="dropdown-basic"
-                        style={{ background: "none", border: "none" }}
-                      >
-                        <img src={human} className="humannlogo" alt="" />
-                      </Dropdown.Toggle>
-
-                      <Dropdown.Menu>
-                        <Dropdown.Item href="/register">Register</Dropdown.Item>
-                        <Dropdown.Item href="/login">Login</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        </nav>
-        <Dialog open={open} onClose={handleClose}>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/3588/3588294.png"
-            style={{ height: "50px", width: "50px", margin: "auto" }}
-            alt="warning"
-            className="noti-icon"
-          />
-
-          <DialogTitle>Delete Account</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              Are you sure you want to delete this Account?
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button
-              variant="contained"
-              color="error"
-              style={{ margin: "auto" }}
-              onClick={deleteAccount}
-            >
-              Delete
-            </Button>
-          </DialogActions>
-          <DialogActions>
-            <Button
-              variant="contained"
-              color="inherit"
-              style={{ margin: "auto" }}
-              onClick={handleClose}
-            >
-              Cancel
-            </Button>
-          </DialogActions>
-        </Dialog>
-
-        <Dialog open={isDeleted} onClose={handleClose}>
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/768px-Flat_tick_icon.svg.png"
-            style={{ height: "50px", width: "50px", margin: "auto" }}
-          />
-
-          <DialogTitle>Account has been deleted</DialogTitle>
-        </Dialog>
-
+      {isAdmin ? (
+          // Render admin side navigation if the user is an admin
+          <>
+            <AdminSideNavigation handleLinkClick={handleLinkClick} />
+            <Container>
+              <Routes>
+                <Route path="/admin" element={<AdminManagement />} />
+                <Route path="/addadmin" element={<AddAdmin />} />
+                <Route path="/getadmin" element={<RetrieveAdmin />} />
+                <Route path="/editadmin/:id" element={<EditAdmin />} />
+                <Route path="/deladmin" element={<DeleteAdmin />} />
+              </Routes>
+            </Container>
+          </>
+        ) : (
+          // Render user side navigation if the user is not an admin
+          <>
+            <UserSideNavigation handleLinkClick={handleLinkClick} />
+            <Container>
+              <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<BikeDash />} />
+                {/* Add other routes for user navigation here */}
+              </Routes>
+            </Container>
+          </>
+        )}
         <Container>
           <Routes>
-            
+
             <Route path="/home" element={<Home />} />
             <Route path="/" element={<BikeDash />} />
             <Route path="/bikeservice" element={<BikeService />} />
@@ -324,207 +202,15 @@ function App() {
             <Route path={"/login"} element={<Login />} />
             <Route path={"/verification"} element={<Verification />} />
             <Route path={"/userdetails/:id"} element={<Userdetails />} />
-            <Route path={"/userhistory/:id"} element={< UserHistory/>} />
+            <Route path={"/userhistory/:id"} element={< UserHistory />} />
             <Route path={"/securitydetails/:id"} element={<Securitydetails />} />
           </Routes>
         </Container>
       </Router>
-      </UserContext.Provider>
+    </UserContext.Provider>
     /* User Side Navigation */
-    // <UserContext.Provider value={{ user, setUser }}>
-    //   <Router>
-    //     <nav className="navigation">
-    //       <Link to="/home" className="brand-name">
-    //         <img
-    //           src={logo}
-    //           className="logo"
-    //           style={{ marginTop: "20px", width: "80%" }}
-    //           alt="Logo"
-    //         />
-    //       </Link>
-    //       <button
-    //         className="hamburger"
-    //         onClick={() => setIsNavExpanded(!isNavExpanded)}
-    //       >
-    //         <svg
-    //           xmlns="http://www.w3.org/2000/svg"
-    //           className="h-5 w-5"
-    //           viewBox="0 0 20 20"
-    //           fill="white"
-    //         >
-    //           <path
-    //             fillRule="evenodd"
-    //             d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-    //             clipRule="evenodd"
-    //           />
-    //         </svg>
-    //       </button>
 
-    //       <div
-    //         className={
-    //           isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-    //         }
-    //       >
-    //         <ul>
-    //           <li>
-    //             <Link to="/home" onClick={handleLinkClick} className="navlink">
-    //               Home
-    //             </Link>
-    //           </li>
-    //           <li>
-    //             <Link
-    //               to="/bikeservice"
-    //               onClick={handleLinkClick}
-    //               className="navlink"
-    //             >
-    //               Bicycles
-    //             </Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/" onClick={handleLinkClick} className="navlink">
-    //               About
-    //             </Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/" onClick={handleLinkClick} className="navlink">
-    //               FAQ
-    //             </Link>
-    //           </li>
-    //           {user && (
-    //             <>
-    //               <li>
-    //                 <Dropdown className="navlink" style={{ marginTop: "10px" }}>
-    //                   <Dropdown.Toggle
-    //                     variant="success"
-    //                     id="dropdown-basic"
-    //                     style={{ background: "none", border: "none" }}
-    //                   >
-    //                     <img src={human} className="humannlogo" alt="" />
-    //                   </Dropdown.Toggle>
 
-    //                   <Dropdown.Menu>
-    //                     <Dropdown.Item>More...</Dropdown.Item>
-    //                     <Dropdown.Item href="/securitydetails/:id">
-    //                       Security Details
-    //                     </Dropdown.Item>
-    //                     <Dropdown.Item href="/userdetails/:id">
-    //                       User Details
-    //                     </Dropdown.Item>
-    //                     <Dropdown.Item onClick={logout}>logout</Dropdown.Item>
-    //                     <Dropdown.Item onClick={handleOpen}>
-    //                       Delete account
-    //                     </Dropdown.Item>
-    //                   </Dropdown.Menu>
-    //                 </Dropdown>
-    //               </li>
-    //             </>
-    //           )}
-    //           {!user && (
-    //             <>
-    //               <li>
-    //                 <Dropdown className="navlink" style={{ marginTop: "10px" }}>
-    //                   <Dropdown.Toggle
-    //                     variant="success"
-    //                     id="dropdown-basic"
-    //                     style={{ background: "none", border: "none" }}
-    //                   >
-    //                     <img src={human} className="humannlogo" alt="" />
-    //                   </Dropdown.Toggle>
-
-    //                   <Dropdown.Menu>
-    //                     <Dropdown.Item href="/register">Register</Dropdown.Item>
-    //                     <Dropdown.Item href="/login">Login</Dropdown.Item>
-    //                   </Dropdown.Menu>
-    //                 </Dropdown>
-    //               </li>
-    //             </>
-    //           )}
-    //         </ul>
-    //       </div>
-    //     </nav>
-    //     <Dialog open={open} onClose={handleClose}>
-    //       <img
-    //         src="https://cdn-icons-png.flaticon.com/512/3588/3588294.png"
-    //         style={{ height: "50px", width: "50px", margin: "auto" }}
-    //         alt="warning"
-    //         className="noti-icon"
-    //       />
-
-    //       <DialogTitle>Delete Account</DialogTitle>
-    //       <DialogContent>
-    //         <DialogContentText>
-    //           Are you sure you want to delete this Account?
-    //         </DialogContentText>
-    //       </DialogContent>
-    //       <DialogActions>
-    //         <Button
-    //           variant="contained"
-    //           color="error"
-    //           style={{ margin: "auto" }}
-    //           onClick={deleteAccount}
-    //         >
-    //           Delete
-    //         </Button>
-    //       </DialogActions>
-    //       <DialogActions>
-    //         <Button
-    //           variant="contained"
-    //           color="inherit"
-    //           style={{ margin: "auto" }}
-    //           onClick={handleClose}
-    //         >
-    //           Cancel
-    //         </Button>
-    //       </DialogActions>
-    //     </Dialog>
-
-    //     <Dialog open={isDeleted} onClose={handleClose}>
-    //       <img
-    //         src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Flat_tick_icon.svg/768px-Flat_tick_icon.svg.png"
-    //         style={{ height: "50px", width: "50px", margin: "auto" }}
-    //       />
-
-    //       <DialogTitle>Account has been deleted</DialogTitle>
-    //     </Dialog>
-
-    //     <Container>
-    //       <Routes>
-    //         <Route path="/home" element={<Home />} />
-    //         <Route path="/" element={<BikeDash />} />
-    //         <Route path="/bikeservice" element={<BikeService />} />
-    //         <Route path="/bike" element={<Bikes />} />
-    //         <Route path="/bikestop" element={<BikeStop />} />
-    //         <Route path="/bikedash" element={<BikeDash />} />
-    //         <Route path="/addbike" element={<AddBikes />} />
-    //         <Route path="/addbikestop" element={<AddBikeStop />} />
-    //         <Route path="/editbike/:id" element={<EditBikes />} />
-    //         <Route path="/editbikestop/:id" element={<EditBikeStop />} />
-    //         <Route path={"/reward"} element={<RewardManagement />} />
-    //         <Route path={"/addreward"} element={<AddReward />} />
-    //         <Route path={"/getreward"} element={<RetrieveReward />} />
-    //         <Route path={"/editreward/:id"} element={<EditReward />} />
-    //         <Route path={"/delreward"} element={<DeleteReward />} />
-    //         <Route path={"/user"} element={<UserManagement />} />
-    //         {/* <Route path={"/adminhome"} element={<AdminHome />} /> */}
-    //         <Route path={"/admin"} element={<AdminManagement />} />
-    //         <Route path={"/addadmin"} element={<AddAdmin />} />
-    //         <Route path={"/getadmin"} element={<RetrieveAdmin />} />
-    //         <Route path={"/editadmin/:id"} element={<EditAdmin />} />
-    //         <Route path={"/deladmin"} element={<DeleteAdmin />} />
-    //         <Route path="/qrcode" element={<QRCode />} />
-    //         <Route path={"/register"} element={<Register />} />
-    //         <Route path={"/login"} element={<Login />} />
-    //         <Route path={"/verification"} element={<Verification />} />
-    //         <Route path={"/userdetails/:id"} element={<Userdetails />} />
-    //         <Route
-    //           path={"/securitydetails/:id"}
-    //           element={<Securitydetails />}
-    //         />
-    //       </Routes>
-    //     </Container>
-    //   </Router>
-    // </UserContext.Provider>
-
-);
+  );
 }
 export default App;
