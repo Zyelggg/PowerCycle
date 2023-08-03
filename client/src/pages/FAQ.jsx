e16215fddfbce6b595b30cbac25bb1f68f056a31
ZhiLong25 <tanzhilong25@gmail.com>
2023-08-03 22:05:09 +0800

    c

--- client/src/pages/FAQ.jsx
+++ client/src/pages/FAQ.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import './styles/FAQ.css';
 
+import { useNavigate, Link } from 'react-router-dom';
+import { Container, AppBar, Toolbar, Typography, Box, Button, Grid } from '@mui/material';
+import instagram from './images/instagram.png'
+import facebook from './images/facebook.png'
+import twitter from './images/twitter.png'
+import logo from './images/powerlogo.png';
+
 const FAQItem = ({ question, answer }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
@@ -52,8 +59,51 @@ const FAQ = () => {
         <FAQItem key={index} question={faq.question} answer={faq.answer} />
       ))}
     </div>
+
+    <AppBar position="static" className="Footer">
+        <Container>
+          <Toolbar disableGutters={true}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={3}>
+                <img src={logo} className="logo" alt="PowerLogo" />
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Typography variant="h6">Navigation</Typography>
+                <Link to="/home">
+                  <Typography>Home</Typography>
+                </Link>
+                <Link to="/bikeservice">
+                  <Typography>Bicycles</Typography>
+                </Link>
+                <Link to="/">
+                  <Typography>About</Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Typography variant="h6">Customer Help</Typography>
+                <Link to="/">
+                  <Typography>FAQ</Typography>
+                </Link>
+                <Link to="/">
+                  <Typography>Message Us</Typography>
+                </Link>
+              </Grid>
+              <Grid item xs={12} md={3}>
+                <Typography variant="h6">Social Media</Typography>
+                <img src={instagram} className='socials'></img>
+                <img src={twitter} className='socials'></img>
+                <img src={facebook} className='socials'></img>
+              </Grid>
+            </Grid>
+          </Toolbar>
+          <Typography variant="body1" style={{ textAlign: "center", marginTop: "40px" }}>
+            © 2023 PowerRide. All rights reserved.
+          </Typography>
+        </Container>
+      </AppBar>
+
     </div>
- 
+  
   );
 };
 
