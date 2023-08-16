251e3b5ce8e04bc2daefaae66dd554dbc11bcd2c
HoldinMcGroins <113585933+HoldinMcGroins@users.noreply.github.com>
2023-08-17 00:12:23 +0800

    Update Weather.jsx

--- client/src/pages/Weather.jsx
+++ client/src/pages/Weather.jsx
@@ -52,11 +52,15 @@ function Weather() {
 
     return (
         <Container>
-            <Typography variant='h2' style={{color:"black;", textAlign: 'left', margin: "20px"}}>Weather Forecast</Typography>
+            <Typography variant='h2' style={{ color: "black;", textAlign: 'left', margin: "20px" }}>Weather Forecast</Typography>
             <Container style={{ margin: "20px" }}>
 
                 <iframe width="100%" height="400" src="https://data.gov.sg/dataset/weather-forecast/resource/571ef5fb-ed31-48b2-85c9-61677de42ca9/view/4c127d9a-cba6-445a-8fc1-978b565f9bf7" frameBorder="0"> </iframe>
+                <Helmet>
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+                    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
 
+                </Helmet>
                 <Accordion>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
@@ -65,12 +69,24 @@ function Weather() {
                     >
                         <Typography>2-Hour Weather Forecast</Typography>
                     </AccordionSummary>
-                    <AccordionDetails>
-
+                    <AccordionDetails style={{height:"200px !important"}}>
+                        {/* <div className="swiper mySwiper" style={{width:"20rem",height:"20rem"}}>
+                            <div className="swiper-wrapper">
                         {weatherData.map((forecast, index) => (
-                            <li key={index}>
+
+                              <div className="swiper-slide" key={index}>
+                                {forecast.forecast.toLowerCase().includes("cloudy") && (
+                                    <img src={cloudy} className='weatherimage' alt="Cloudy" />
+                                )}
+                                {forecast.forecast.toLowerCase().includes("light showers") && !forecast.forecast.toLowerCase().includes("showers") && (
+                                    <img src={lightshowers} className='weatherimage' alt="Light Showers" />
+                                )}
+                                {forecast.forecast.toLowerCase().includes("showers") && (
+                                    <img src={showers} className='weatherimage' alt="Showers" />
+                                )}
                                 {forecast.area}: {forecast.forecast}
-                            </li>
+                            </div>
+                            
                         ))}
                         </div>
                             </div> */}
@@ -113,54 +129,13 @@ function Weather() {
                 </Swiper>
                     </AccordionDetails>
                 </Accordion>
-
+                
 
             </Container>
 
 
 
-            <AppBar position="static" className="Footer">
-                <Container>
-                    <Toolbar disableGutters={true}>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12} md={3}>
-                                <img src={logo} className="logo" alt="PowerLogo" />
-                            </Grid>
-                            <Grid item xs={12} md={3}>
-                                <Typography variant="h6">Navigation</Typography>
-                                <Link to="/home">
-                                    <Typography>Home</Typography>
-                                </Link>
-                                <Link to="/bikeservice">
-                                    <Typography>Bicycles</Typography>
-                                </Link>
-                                <Link to="/">
-                                    <Typography>About</Typography>
-                                </Link>
-                            </Grid>
-                            <Grid item xs={12} md={3}>
-                                <Typography variant="h6">Customer Help</Typography>
-                                <Link to="/">
-                                    <Typography>FAQ</Typography>
-                                </Link>
-                                <Link to="/">
-                                    <Typography>Message Us</Typography>
-                                </Link>
-                            </Grid>
-                            <Grid item xs={12} md={3}>
-                                <Typography variant="h6">Social Media</Typography>
-                                <img src={instagram} className='socials'></img>
-                                <img src={twitter} className='socials'></img>
-                                <img src={facebook} className='socials'></img>
-                            </Grid>
-                        </Grid>
-                    </Toolbar>
-                    <Typography variant="body1" style={{ textAlign: "center", marginTop: "40px" }}>
-                        © 2023 PowerRide. All rights reserved.
-                    </Typography>
-                </Container>
-            </AppBar>
-
+            
         </Container>
 
     );
